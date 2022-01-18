77f2713691665a0c62e54b20384634862b5062f9
Return0Zero <69299788+Return0Zero@users.noreply.github.com>
2022-01-18 02:56:58 -0500

    form test 2

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -4,12 +4,12 @@ import './form.css';
 const Form = () => {
     return (
         <form className='form--container' name="contact-form" method="POST"  data-netlify="true">
-            
-                <input className='input' type='text' placeholder='Name' name='name' required/>
-                <input className='input' type='email' placeholder='Email' name='emil' required/>
-                <input className='input' type='text' placeholder='Subject' name='subject' required/>
-                <textarea className='input' type='text' placeholder='Message' name='message' required/>
-                <input className='input-button' type='submit' value='Submit' required/>
+            <input type="hidden" name='form-name' value="contact-form"></input>
+            <input className='input' type='text' placeholder='Name' name='name' required/>
+            <input className='input' type='email' placeholder='Email' name='emil' required/>
+            <input className='input' type='text' placeholder='Subject' name='subject' required/>
+            <textarea className='input' type='text' placeholder='Message' name='message' required></textarea>
+            <input className='input-button' type='submit' required/>
         </form>
     )
 }
