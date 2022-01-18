553565d9bb13d8609807822ddeabdc7eb823f547
Return0Zero <69299788+Return0Zero@users.noreply.github.com>
2022-01-18 02:33:39 -0500

    form

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -3,26 +3,26 @@ import './form.css';
 
 const Form = () => {
     return (
-        <form className='form--container' name="contact" method="POST"  data-netlify="true">
+        <form className='form--container' name="contact-form" method="POST"  data-netlify="true">
             <div>
                 <label hidden>Name</label>
-                <input className='input' type='text' placeholder='Name' required/>
+                <input className='input' type='text' placeholder='Name' name='name' required/>
             </div>
             <div>
                 <label hidden>Email</label>
-                <input className='input' type='text' placeholder='Email' required/>
+                <input className='input' type='email' placeholder='Email' name='emil' required/>
             </div>
             <div>
                 <label hidden>Subject</label>
-                <input className='input' type='text' placeholder='Subject' required/>
+                <input className='input' type='text' placeholder='Subject' name='subject' required/>
             </div>
             <div>
                 <label hidden>Message</label>
-                <input className='input' type='text' placeholder='Message' required/>
+                <textarea className='input' type='text' placeholder='Message' name='message' required/>
             </div>
             <div>
                 <label hidden>Submit</label>
-                <input className='input-button' type='button' value='Submit' required/>
+                <input className='input-button' type='submit' value='Submit' required/>
             </div>
         </form>
     )
