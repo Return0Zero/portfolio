0fb7ccef88af4d5064d98a578bbf982ee633c15e
Return0Zero <69299788+Return0Zero@users.noreply.github.com>
2022-01-27 06:39:02 -0500

    Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
+import ReactGa from 'react-ga';
 
 import { About, Contact, Header, Navigation, Projects, Skills, CTA, Footer } from './containers';
 
 
 export const App = () => {
+
+    useEffect(() => {
+        ReactGa.initialize('G-CC571RXN32')
+        ReactGa.pageview('/')
+    }, [])
+
     return (
         
         <div className='App'>
